9fff159913e049b64f7e68d82007a88beea57779
Vasily <vasily_pavlov_98@mail.ru>
2022-11-20 17:32:00 +0700

    implementation commands for application layer

---
+++ Geodata.Application/Geodata/Commands/CreateGeodata/CreateGeodataCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Geodata.Application.Geodata.Commands.CreateGeodata;
+
+public class CreateGeodataCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public string Details { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+}
---
+++ Geodata.Domain/GeodataDomain.cs
@@ -0,0 +1,17 @@
+﻿namespace Geodata.Domain;
+
+public class GeodataDomain
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+    public string Title { get; set; }
+    public string? Details { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    /// <summary>
+    /// Checked by moderator
+    /// </summary>
+    public bool IsChecked { get; set; }
+}
